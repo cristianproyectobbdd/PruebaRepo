b8ef88db7883fd8cd40dc5b6ee46766754916a96
cristianproyectobbdd <cristianperezleon695@gmail.com>
2023-12-11 18:22:11 +0100

    probandoPat

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+/**
+ *
+ * @author cristian
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        
+        String nombre = "Cristian";
+        int edad=28;
+        System.out.println("Hola Mundo! Mi nombre es "+nombre + " y tengo "+edad+" años");
+    }
+    
+}
